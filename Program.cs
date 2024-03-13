db98e5e28c87c907065b5be07eb41ce2c7bfa28f
Murilo Barbosa <muriloab96@gmail.com>
2024-03-12 22:30:13 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Target_Sistemas___Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
