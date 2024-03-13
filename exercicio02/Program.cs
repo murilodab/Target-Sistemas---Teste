af1c4fefa12ca9e1ffa98a3b58fd019a740d7ca8
Murilo Barbosa <108987670+murilodab@users.noreply.github.com>
2024-03-12 22:32:43 -0300

    Added ex 01, 02, 05

---
+++ exercicio02/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número: \n");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+
+            List<int> fibonacci = generateFibonacci(num);
+
+            foreach(int n in fibonacci)
+            {
+                Console.Write(n);
+            }
+
+            if (fibonacci.Contains(num))
+            {
+                Console.WriteLine("\n\nO seu número está no Fibonacci.");
+            }else
+            {
+                Console.WriteLine("\n\nO seu número não está no Fibonacci.");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<int> generateFibonacci(int num) {
+
+            List<int> fibonacci = new List<int>
+            {
+                0,1
+            };
+
+            int i = 0;
+
+            while (fibonacci[i+1] < num)
+            {
+                fibonacci.Add(fibonacci[i] + fibonacci[i+1]);
+                i++;
+            }
+
+            return fibonacci;
+
+
+        }
+
+    }
+}
---
+++ exercicio05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício 05 - Inverta uma string\n\n");
+            Console.WriteLine("Escreva a palavra ou frase para ser invertida: ");
+
+            string input = Console.ReadLine();
+
+            string invertida = invertString(input);
+            Console.WriteLine(invertida);
+            Console.ReadLine();
+        }
+
+        public static string invertString(string input)
+        {
+
+
+
+            char[] invertida = input.ToCharArray();
+            Array.Reverse(invertida);
+
+            return new string(invertida);
+
+        }
+    }
+}
